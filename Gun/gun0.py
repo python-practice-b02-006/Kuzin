9f91c359d14e9441ad2420562b92ae0bbf82b5eb
Ros Kuz <kuzin.rf@phystech.edu>
2020-10-23 15:44:28 +0700

    Add ball movement

--- Gun/gun0.py
+++ Gun/gun0.py
@@ -1,45 +1,116 @@
 import numpy as np
 import pygame as pg
-from random import randint, gauss
+from random import randint
 
 pg.init()
-pg.font.init()
 
 WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 RED = (255, 0, 0)
+BLUE = (0, 0, 255)
 
 SCREEN_SIZE = (800, 600)
 
 
 class Gun():
+    def __init__(self, coord=[30, SCREEN_SIZE[1]//2]):
+        self.coord = coord
+        self.angle = 0
 
-    pass    
+    def draw(self, screen):
+        gun_shape = []
+        v1 = np.array([int(5*np.cos(self.angle - np.pi/2)), int(5*np.sin(self.angle - np.pi/2))])
+        v2 = np.array([int(20*np.cos(self.angle)), int(20*np.sin(self.angle))])
+        gun_pos = np.array(self.coord)
+        gun_shape.append((gun_pos + v1).tolist())
+        gun_shape.append((gun_pos + v1 + v2).tolist())
+        gun_shape.append((gun_pos + v2 - v1).tolist())
+        gun_shape.append((gun_pos - v1).tolist())
+        pg.draw.polygon(screen, BLUE, gun_shape)
+
+    def set_angle(self, mouse_pos):
+        self.angle = np.arctan2(mouse_pos[1] - self.coord[1], 
+                                mouse_pos[0] - self.coord[0])   
+    
+class Ball():
+    def __init__(self, coord, vel, rad=15, color=None):
+        if color == None:
+            color = (randint(0, 255), randint(0, 255), randint(0, 255))
+        self.color = color
+        self.coord = coord
+        self.vel = vel
+        self.rad = rad
+        
+    def draw(self, screen):
+        pg.draw.circle(screen, self.color, self.coord, self.rad)
+    
+    def move(self, t_step=1.):
+        for i in range(2):
+            self.coord[i] += int(self.vel[i] * t_step)
+        self.check_walls()
+        
+    def check_walls(self):
+        n = [[1, 0], [0, 1]]
+        for i in range(2):
+            if self.coord[i] < self.rad:
+                self.coord[i] = self.rad
+                self.flip_vel(n[i])
+            elif self.coord[i] > SCREEN_SIZE[i] - self.rad:
+                self.coord[i] = SCREEN_SIZE[i] - self.rad
+                self.flip_vel(n[i])
+                
+    def flip_vel(self, axis, coef_perp = 1., coef_par = 1.):
+        vel = np.array(self.vel)
+        n = np.array(axis)
+        n = n / np.linalg.norm(n)
+        vel_perp = vel.dot(n) * n
+        vel_par = vel - vel_perp
+        ans = -vel_perp * coef_perp + vel_par * coef_par
+        self.vel = ans.astype(np.int).tolist()
+    
 class Table():
     pass
     
-class Ball():
-    pass
-
 class Manager():
 
     def __init__(self):
         self.gun = Gun()
-        self.score_t = Table()
-        
-    def draw(self, screen):
-        screen.fill(BLACK)
+        self.table = Table()
+        self.balls = []
+        self.balls.append(Ball([100, 100], [10, 20]))
         
     def process(self, events, screen):
         done = self.handle_events(events)
+        self.move()
         self.draw(screen)
         return done
+             
+    def draw(self, screen):
+        screen.fill(BLACK)
+        for ball in self.balls:
+            ball.draw(screen)
+        self.gun.draw(screen)
+        
+    def move(self):
+        for ball in self.balls:
+            ball.move()
+
         
     def handle_events(self, events):
         done = False
         for event in events:
             if event.type == pg.QUIT:
                 done = True
+            elif event.type == pg.KEYDOWN:    
+                if event.key == pg.K_UP:
+                    self.gun.coord[1] -= 5
+                elif event.key == pg.K_DOWN:
+                    self.gun.coord[1] += 5
+        
+        if pg.mouse.get_focused():
+            mouse_pos = pg.mouse.get_pos()
+            self.gun.set_angle(mouse_pos)
+            
         return done
 
 
@@ -54,7 +125,6 @@ mgr = Manager()
 
 while not done:
     clock.tick(15)
-    screen.fill(BLACK)
 
     done = mgr.process(pg.event.get(), screen)
 
